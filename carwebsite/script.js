355c45306ab0c766049908314e1f9b94623aa602
namadee <65288221+namadee@users.noreply.github.com>
2021-02-08 12:23:03 +0530

    Update script.js

--- carwebsite/script.js
+++ carwebsite/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 $("#demo").vc3dEye({
-imagePath:"images/360/", // the location where you’ve put the images.
+imagePath:"images/", // the location where you’ve put the images.
 totalImages:45, // the number of images you have.
 imageExtension:"png" // the extension of the images. Make sure all the images have same extension.
 });
