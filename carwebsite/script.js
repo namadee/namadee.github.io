923bb2be7bfc49d60a21f1578924513d9cabf1e9
namadee <65288221+namadee@users.noreply.github.com>
2021-02-08 11:35:25 +0530

    project1

---
+++ carwebsite/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+$("#demo").vc3dEye({
+imagePath:"images/360/", // the location where you’ve put the images.
+totalImages:45, // the number of images you have.
+imageExtension:"png" // the extension of the images. Make sure all the images have same extension.
+});
+
+});
